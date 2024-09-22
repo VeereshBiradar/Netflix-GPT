cdbbd2f33dc3ff118931f065e0964f5cb919d9e9
Veeresh Biradar <veeresh.r.biradar@outlook.com>
2024-09-22 20:34:16 +0530

    added a redux store

---
+++ src/utils/userSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState: null,
+    reducers: {
+        addUser: (state, action)=> {
+
+        },
+        removeUser: (state, action)=> {
+
+        }
+    }
+});
+
+export default userSlice;
